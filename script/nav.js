c7ea4d74c2bb75f14da7acbec0483523a9e5fe13
Deuteronome <o.burcker@e2c-grandlille.fr>
2023-11-23 11:51:33 +0100

    menu glissant fini

--- script/nav.js
+++ script/nav.js
@@ -2,11 +2,27 @@
 
 let nav = document.getElementById("navBar")
 let navButton = document.getElementById("navTrigger")
+let main = document.getElementById('javaMain')
+
+/*on crée de variables utiles*/
+
+let isHidden = true
 
 /*on définit les fonctions*/
 
 function moveNav() {
-    console.log('tout est ok')
+    if(isHidden) {
+        nav.style.transform ="translateX(20vw)"
+        main.style.width = "80vw"
+        main.style.left = "20vw"
+        isHidden = false
+    } else {
+        nav.style.transform ="translateX(-20vw)"
+        main.style.width = "100vw"
+        main.style.left = "0"
+        isHidden = true
+    }
+    
 }
 
 //on règle les évènements déclaencheur
